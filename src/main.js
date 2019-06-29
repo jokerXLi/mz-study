521fe5c066288bea51def74fd015f498449a31a9
lixinglong <lixinglong@qq.com>
2019-06-29 18:52:47 +0800

    修改main.js引入App的路径错误

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import Vue from "vue";
 import router from "./router";
 import store from './store'
-import APP from "./APP";
+import APP from "./App";
 import './assets/style/base.scss'
 
 new Vue({
